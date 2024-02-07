8997b6fbce0611b0c6b6b1afe46c1b426967e75d
holus-bolus <ustinovoleksij@gmail.com>
2024-02-07 14:34:57 +0200

    Fixed the bug цшер еÑwith the label

--- src/containers/guest-home-page/forgot-password/ForgotPassword.jsx
+++ src/containers/guest-home-page/forgot-password/ForgotPassword.jsx
@@ -94,7 +94,7 @@ const ForgotPassword = () => {
           onChange={handleInputChange('email')}
           required
           size='large'
-          sx={{ mb: '5px' }}
+          sx={{ mb: '5px', mt: '15px' }}
           type='email'
           value={data.email}
         />
