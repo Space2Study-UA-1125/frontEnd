bc8dc7ee7b0e8aea983fb214c1cf168badd36f34
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-02-08 10:42:19 +0200

    Logo button route

--- src/containers/logo/Logo.jsx
+++ src/containers/logo/Logo.jsx
@@ -1,9 +1,18 @@
 import Box from '@mui/material/Box'
 import logoLight from '~/assets/logo-light.svg'
 import logo from '~/assets/logo.svg'
+import { Link } from 'react-router-dom'
+import { guestRoutes } from '~/router/constants/guestRoutes'
 
 const Logo = ({ light = false, ...props }) => (
-  <Box alt='logo' component={'img'} src={light ? logoLight : logo} {...props} />
+  <Link to={guestRoutes.home.path}>
+    <Box
+      alt='logo'
+      component={'img'}
+      src={light ? logoLight : logo}
+      {...props}
+    />
+  </Link>
 )
 
 export default Logo
