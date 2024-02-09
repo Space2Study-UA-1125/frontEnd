8a2c35f3cf6a2d02e8d7fa0e5fd1291222fc0d7f
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-02-09 13:28:13 +0200

    onClicFunction

--- src/containers/layout/navbar/NavBar.jsx
+++ src/containers/layout/navbar/NavBar.jsx
@@ -51,6 +51,10 @@ const Navbar = () => {
     openDrawer()
   }
 
+  // scrollToTheTop(() => {
+  //   wellcomeRef.current?.scrollTo(0, 0)
+  // })
+
   const navigationList = navigationItems.map((item, idx, array) => {
     const isLast = array.length - 1 === idx
     const isActive = Boolean(matchPath(item.path, pathname))
