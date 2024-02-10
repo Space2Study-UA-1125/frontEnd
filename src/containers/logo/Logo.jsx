9312a0f2fd5651adb9c22f35c9b186d802fb985f
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-02-10 16:47:05 +0200

    logo with hashLink

--- src/containers/logo/Logo.jsx
+++ src/containers/logo/Logo.jsx
@@ -3,14 +3,7 @@ import logoLight from '~/assets/logo-light.svg'
 import logo from '~/assets/logo.svg'
 
 const Logo = ({ light = false, ...props }) => (
-  <div>
-    <Box
-      alt='logo'
-      component={'img'}
-      src={light ? logoLight : logo}
-      {...props}
-    />
-  </div>
+  <Box alt='logo' component={'img'} src={light ? logoLight : logo} {...props} />
 )
 
 export default Logo
