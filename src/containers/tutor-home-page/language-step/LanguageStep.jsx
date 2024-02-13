bc4bbb357fc98928a4381ac5001a2533542112c6
holus-bolus <ustinovoleksij@gmail.com>
2024-02-13 16:18:11 +0200

    Rewrote tests

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -4,7 +4,7 @@ import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.
 
 const LanguageStep = ({ btnsBox }) => {
   return (
-    <Box sx={styles.container}>
+    <Box data-testid='language-step-container' sx={styles.container}>
       Language step
       {btnsBox}
     </Box>
--- tests/unit/containers/tutor-home-page/language-step/LanguageStep.spec.jsx
+++ tests/unit/containers/tutor-home-page/language-step/LanguageStep.spec.jsx
@@ -1,19 +1,40 @@
+// import React from 'react'
+// import { render, screen } from '@testing-library/react'
+// import '@testing-library/jest-dom/extend-expect'
+// import LanguageStep from '../../../../../src/containers/tutor-home-page/language-step/LanguageStep'
+//
+// describe('LanguageStep Component', () => {
+//   test('renders the "Language step" text', () => {
+//     render(<LanguageStep />)
+//     const textElement = screen.getByText(/Language step/i)
+//     expect(textElement).toBeInTheDocument()
+//   })
+//
+//   test('renders the content passed through the btnsBox prop', () => {
+//     const btnsBoxContent = <div>Test Button</div>
+//     render(<LanguageStep btnsBox={btnsBoxContent} />)
+//     const btnBoxElement = screen.getByText(/Test Button/i)
+//     expect(btnBoxElement).toBeInTheDocument()
+//   })
+// })
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import LanguageStep from '../../../../../src/containers/tutor-home-page/language-step/LanguageStep'
 
 describe('LanguageStep Component', () => {
-  test('renders the "Language step" text', () => {
+  // Scenario: Container should be rendered
+  test('renders the LanguageStep container', () => {
     render(<LanguageStep />)
-    const textElement = screen.getByText(/Language step/i)
-    expect(textElement).toBeInTheDocument()
+    const containerElement = screen.getByTestId('language-step-container')
+    expect(containerElement).toBeInTheDocument()
   })
 
-  test('renders the content passed through the btnsBox prop', () => {
-    const btnsBoxContent = <div>Test Button</div>
+  // Scenario: Check if the buttons passed in props are in the document
+  test('renders the buttons passed through the btnsBox prop', () => {
+    const btnsBoxContent = <button>Test Button</button> // Updated for button specificity
     render(<LanguageStep btnsBox={btnsBoxContent} />)
-    const btnBoxElement = screen.getByText(/Test Button/i)
+    const btnBoxElement = screen.getByRole('button', { name: /Test Button/i })
     expect(btnBoxElement).toBeInTheDocument()
   })
 })
