62992781e10695e720c38854588a16d185feb3b4
Stas <49125203+StasRusnak1@users.noreply.github.com>
2024-02-15 20:24:37 +0200

    Update tests/unit/components/loader/Loader.spec.jsx
    Co-authored-by: vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>

--- tests/unit/components/loader/Loader.spec.jsx
+++ tests/unit/components/loader/Loader.spec.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Loader from '~/components/loader/Loader'
 
 describe('Loader test', () => {
-  test('should be in the document', () => {
+  it ('should be in the document', () => {
     render(<Loader />)
     const loaderElement = screen.getByTestId('loader')
     expect(loaderElement).toBeInTheDocument()
