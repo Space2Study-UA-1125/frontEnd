0a44fd55f9ee09e24735161c4a8458f5e4c2aa43
Anastasiia Ivanchenko <iva21sta@gmail.com>
2024-02-15 13:11:57 +0200

    Add MUI component mocks

--- tests/unit/components/title-with-description/TitleWithDescription.spec.jsx
+++ tests/unit/components/title-with-description/TitleWithDescription.spec.jsx
@@ -1,6 +1,15 @@
 import { screen, render } from '@testing-library/react'
 import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
 
+vi.mock('@mui/material', async () => {
+  const actual = await vi.importActual('@mui/material')
+  return {
+    ...actual,
+    Box: vi.fn(({ children, sx }) => <div style={sx}>{children}</div>),
+    Typography: vi.fn(({ children, sx }) => <p style={sx}>{children}</p>)
+  }
+})
+
 describe('TitleWithDescription component', () => {
   const props = {
     title: 'Short Title',
