99d4a7f44578de312964c4b3fba598cdeab406c0
Stas <cnfchecyfr@gmail.com>
2024-02-15 22:40:53 +0200

    Added test for AccordionWithImage component

---
+++ tests/unit/components/accordion-with-image/AccordionWithImage.spec.jsx
@@ -0,0 +1,34 @@
+import { render, fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
+import AccordionWithImage from '~/components/accordion-with-image/AccordionWithImage'
+import MapLogo from '~/assets/img/guest-home-page/map.svg'
+
+vi.mock('~/components/accordion/Accordions', () => ({
+  __esModule: true,
+  default: function ({ items, activeIndex, onChange }) {
+    return (
+      <div>
+        {items.map((item, index) => (
+          <button key={index} onClick={() => onChange(index)}>
+            {item.title}
+          </button>
+        ))}
+        <div>{items[activeIndex].description}</div>
+      </div>
+    )
+  }
+}))
+
+describe('AccordionWithImage test', () => {
+  const items = [
+    { title: 'Title 1', description: 'Description 1', image: MapLogo },
+    { title: 'Title 2', description: 'Description 2', image: MapLogo }
+  ]
+
+  it('should open content on title click', () => {
+    const { getByText } = render(<AccordionWithImage items={items} />)
+
+    fireEvent.click(getByText('Title 2'))
+    expect(getByText('Description 2')).toBeInTheDocument()
+  })
+})
