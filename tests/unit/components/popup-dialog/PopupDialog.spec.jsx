a4d6c07b98f54e7f5ce58a24fde67d209e645862
Ashfox21 <ashfox201@gmail.com>
2024-02-19 13:51:43 +0200

    Rewrote the test according to the requested changes

--- tests/unit/components/popup-dialog/PopupDialog.spec.jsx
+++ tests/unit/components/popup-dialog/PopupDialog.spec.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import PopupDialog from '../../../../src/components/popup-dialog/PopupDialog'
+import PopupDialog from '~/components/popup-dialog/PopupDialog'
 import { vi } from 'vitest'
 
 describe('PopupDialog component', () => {
@@ -17,12 +17,12 @@ describe('PopupDialog component', () => {
     render(<PopupDialog {...props} />)
   })
 
-  it('Component renders correctly', () => {
+  it('renders correctly', () => {
     const contentElement = screen.getByText('TestContent')
     expect(contentElement).toBeInTheDocument()
   })
 
-  it('Dialog closes when you click on the close button', async () => {
+  it('closes when you click on the close button', async () => {
     const closeElement = screen.getByTestId('CloseIcon')
 
     await userEvent.click(closeElement)
@@ -30,7 +30,7 @@ describe('PopupDialog component', () => {
     expect(props.closeModal).toHaveBeenCalled()
   })
 
-  it('Dialog closes with 5 seconds of inactivity', async () => {
+  it('closes with 5 seconds of inactivity', () => {
     const popupBoxElement = screen.getByTestId('popupContent')
 
     userEvent.hover(popupBoxElement)
