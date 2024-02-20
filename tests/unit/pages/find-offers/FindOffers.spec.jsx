e405fb4ffc5f663b0d48989e4ae39022213c9b77
Vladyslava Korogodova <korogodova.vladislava@gmail.com>
2024-02-20 20:09:44 +0200

    Mocked PageWrapper and change imports with suggestion

--- tests/unit/pages/find-offers/FindOffers.spec.jsx
+++ tests/unit/pages/find-offers/FindOffers.spec.jsx
@@ -1,15 +1,24 @@
 import { screen } from '@testing-library/react'
 import { renderWithProviders } from '~tests/test-utils'
-import { it } from 'vitest'
 
 import FindOffers from '~/pages/find-offers/FindOffers'
 
+vi.mock('~/components/page-wrapper/PageWrapper', () => ({
+  __esModule: true,
+  default: function ({ children }) {
+    return <div>{children}</div>
+  }
+}))
 describe('FindOffersPage test', () => {
   beforeEach(() => {
     renderWithProviders(<FindOffers />)
   })
 
-  it('text of the page must be in the document', () => {
-    expect(screen.getByText(/Find offers/)).toBeInTheDocument()
+  it('renders the page successfully', () => {
+    expect(screen.getByText(/Find offers/i)).toBeInTheDocument()
+  })
+
+  it('includes the "Find offers" text', () => {
+    expect(screen.getByText('Find offers')).toBeTruthy()
   })
 })
