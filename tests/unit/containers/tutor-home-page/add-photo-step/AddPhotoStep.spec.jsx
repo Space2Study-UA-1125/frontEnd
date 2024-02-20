ed09d3521da9e594dbf29b05d099bd7f419989bb
iaremchuk <ya.olya24@gmail.com>
2024-02-20 01:14:15 +0000

    Add unit test for AddPhotoStep component/add mocked component

--- tests/unit/containers/tutor-home-page/add-photo-step/AddPhotoStep.spec.jsx
+++ tests/unit/containers/tutor-home-page/add-photo-step/AddPhotoStep.spec.jsx
@@ -1,22 +1,22 @@
 import { render, screen } from '@testing-library/react'
+import { it, vi } from 'vitest'
 import AddPhotoStep from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep'
 
+vi.mock('@mui/material', () => ({
+    Box: vi.fn(({ children }) => <div data-testid='addPhotoStepContainer'>{children}</div>)
+}))
+
+const prop = {
+    btnsBox: <div data-testid='addPhotoStepBtns' />
+}
+
+beforeEach(() => {
+    render(<AddPhotoStep {...prop} />)
+})
+
 describe('AddPhotoStep component', () => {
-    const prop = {
-        btnsBox: <div data-testid='addPhotoStepBtn' />
-    }
-
-    beforeEach(() => {
-        render(<AddPhotoStep {...prop} />)
-    })
-
-    it('renders container', () => {
-        const addPhotoStepContainer = screen.getByTestId('AddPhotoStepContainer')
-        expect(addPhotoStepContainer).toBeInTheDocument()
-    })
-
-    it('renders button that is passed in props', () => {
-        const addPhotoStepBtn = screen.getByTestId('addPhotoStepBtn')
-        expect(addPhotoStepBtn).toBeInTheDocument()
+    it('renders container with the buttons passed in props', () => {
+        const addPhotoStepBtns = screen.getByTestId('addPhotoStepBtns')
+        expect(addPhotoStepBtns).toBeInTheDocument()
     })
 })
