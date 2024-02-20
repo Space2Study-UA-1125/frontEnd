5aad95e4b8fe261eee95b0e1db7cc9e406760499
iaremchuk <ya.olya24@gmail.com>
2024-02-20 01:14:15 +0000

    Add unit test for AddPhotoStep component/add mocked component

--- src/containers/tutor-home-page/add-photo-step/AddPhotoStep.jsx
+++ src/containers/tutor-home-page/add-photo-step/AddPhotoStep.jsx
@@ -1,10 +1,9 @@
 import { Box } from '@mui/material'
-
 import { style } from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep.style'
 
 const AddPhotoStep = ({ btnsBox }) => {
   return (
-    <Box data-testid='AddPhotoStepContainer' sx={style.root}>
+    <Box sx={style.root}>
       AddPhoto step
       {btnsBox}
     </Box>
