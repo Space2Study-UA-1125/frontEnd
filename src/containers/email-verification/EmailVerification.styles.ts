8ea8c67907920d0b7615b8efc24a3502f6a7a9d4
holus-bolus <ustinovoleksij@gmail.com>
2024-02-21 10:44:53 +0200

    Changed paths in the unit tests and styles

--- src/containers/email-verification/EmailVerification.styles.ts
+++ src/containers/email-verification/EmailVerification.styles.ts
@@ -1,4 +1,5 @@
 import { SxProps, Theme } from '@mui/system'
+
 export const styles: SxProps<Theme> = {
   position: 'absolute',
   top: '50%',
@@ -9,9 +10,18 @@ export const styles: SxProps<Theme> = {
   boxShadow: 24,
   borderRadius: 2,
   textAlign: 'center',
-  // Custom padding
   pt: '35px',
   pr: '110px',
   pb: '100px',
   pl: '110px'
+}
+
+export const typographyStyles: SxProps<Theme> = {
+  mt: 2,
+  width: '525px',
+  mx: 'auto',
+  textAlign: 'center',
+  '& span': {
+    fontWeight: '500'
+  }
 }
--- src/containers/email-verification/EmailVerification.tsx
+++ src/containers/email-verification/EmailVerification.tsx
@@ -4,12 +4,13 @@ import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
 import IconButton from '@mui/material/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
-import { styles } from './EmailVerification.styles'
+import { styles, typographyStyles } from './EmailVerification.styles'
 
 interface Props {
-  open: boolean,
+  open: boolean
   handleClose: () => void
 }
+
 const EmailVerification: React.FC<Props> = ({ open, handleClose }) => {
   return (
     <Modal
@@ -35,25 +36,13 @@ const EmailVerification: React.FC<Props> = ({ open, handleClose }) => {
         </IconButton>
         <Box
           component='img'
-          src='/src/assets/img/email-needs-verified/email-needs-verified.svg'
+          src='~/assets/img/email-needs-verified/email-needs-verified.svg'
           sx={{ height: 100, mx: 'auto', my: 2 }}
         />
         <Typography component='h2' id='confirmation-modal-title' variant='h6'>
           Your email address needs to be verified
         </Typography>
-        <Typography
-          id='confirmation-modal-description'
-          sx={{
-            mt: 2,
-            width: '525px',
-            mx: 'auto',
-            textAlign: 'center',
-
-            '& span': {
-              fontWeight: '500'
-            }
-          }}
-        >
+        <Typography id='confirmation-modal-description' sx={typographyStyles}>
           We sent a verification link to: <span>john.doe@gmail.com</span>. Check
           your email and click on the confirmation button to continue.
         </Typography>
