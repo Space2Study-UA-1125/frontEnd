64d745d5076cb043f38be18393fdd9424b031a01
Wiret02 <chuwachok6@gmail.com>
2024-02-22 12:44:47 +0200

    Language step in progress

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -1,12 +1,22 @@
+import { Typography } from '@mui/material'
 import Box from '@mui/material/Box'
-
+// import { languages } from './constants'
+import languageStepImg from '~/assets/img/tutor-home-page/become-tutor/languages.svg'
 import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.styles'
 
 const LanguageStep = ({ btnsBox }) => {
   return (
     <Box sx={styles.container}>
-      Language step
-      {btnsBox}
+      <Box sx={styles.image}>
+        <img alt='Language Step Image' src={languageStepImg} />
+      </Box>
+      <Box>
+        <Typography sx={styles.infoContainer}>
+          Velit officia consequat duis enim velit mollit. Other categories you
+          can add in your account settings later.
+        </Typography>
+        {btnsBox}
+      </Box>
     </Box>
   )
 }
--- src/containers/tutor-home-page/language-step/LanguageStep.styles.js
+++ src/containers/tutor-home-page/language-step/LanguageStep.styles.js
@@ -7,5 +7,12 @@ export const styles = {
     gap: '40px',
     height: { sm: '485px' },
     ...fadeAnimation
+  },
+  image: {
+    maxWidth: '395',
+    maxheight: '440'
+  },
+  infoContainer: {
+    maxWidth: '432px'
   }
 }
