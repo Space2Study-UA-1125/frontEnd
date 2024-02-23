b323f9d29b18fba13ab97d1f5c46918ec53309d6
Yukhym Rybenchuk <60313835+Wiret02@users.noreply.github.com>
2024-02-23 16:57:10 +0200

    Feature/#151/sp 2 add language step (#167)
    * Language step in progress
    
    * #151 Implemented LanguageStep component with tests for it
    
    * SelfReview: changed for a correct way to use translations
    
    * Add describe block for tests
    
    * Changed a test for more presice work

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -1,12 +1,86 @@
-import Box from '@mui/material/Box'
-
+import {
+  Box,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography
+} from '@mui/material'
+import ClearRoundedIcon from '@mui/icons-material/ClearRounded'
+import { languages } from './constants'
+import { useTranslation } from 'react-i18next'
+import languageStepImg from '~/assets/img/tutor-home-page/become-tutor/languages.svg'
 import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.styles'
+import { useState } from 'react'
 
 const LanguageStep = ({ btnsBox }) => {
+  const { t } = useTranslation()
+
+  const [selectedLanguage, setSelectedLanguage] = useState('')
+
+  const handleChange = (event) => {
+    setSelectedLanguage(event.target.value)
+  }
+
+  const handleClear = () => {
+    setSelectedLanguage('')
+  }
+
   return (
     <Box sx={styles.container}>
-      Language step
-      {btnsBox}
+      <Box sx={styles.languageImage}>
+        <img
+          alt='Language Step Image'
+          src={languageStepImg}
+          style={styles.img}
+        />
+      </Box>
+      <Box sx={styles.infoWrapper}>
+        <Box>
+          <Typography sx={styles.infoDescription}>
+            {t('becomeTutor.languages.title')}
+          </Typography>
+          <Box sx={styles.smallLanguageImage}>
+            <img
+              alt='Language Step Image'
+              src={languageStepImg}
+              style={styles.img}
+            />
+          </Box>
+          <FormControl fullWidth>
+            <InputLabel id='nativeLanguageLabel'>
+              {t('becomeTutor.languages.autocompleteLabel')}
+            </InputLabel>
+            <Select
+              endAdornment={
+                selectedLanguage && (
+                  <IconButton
+                    aria-label='Clear'
+                    edge='end'
+                    onClick={handleClear}
+                    sx={styles.clearIconButton}
+                  >
+                    <ClearRoundedIcon />
+                  </IconButton>
+                )
+              }
+              id='nativeLanguageSelect'
+              label={t('becomeTutor.languages.autocompleteLabel')}
+              labelId='nativeLanguageLabel'
+              onChange={handleChange}
+              value={selectedLanguage}
+            >
+              {languages.map((language) => (
+                <MenuItem key={language} value={language}>
+                  {language}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box>{btnsBox}</Box>
+      </Box>
     </Box>
   )
 }
