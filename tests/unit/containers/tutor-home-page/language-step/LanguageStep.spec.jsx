5d40aecc58ab50e911437a88f895c088be816c49
Wiret02 <chuwachok6@gmail.com>
2024-02-23 14:58:52 +0200

    Add describe block for tests

--- tests/unit/containers/tutor-home-page/language-step/LanguageStep.spec.jsx
+++ tests/unit/containers/tutor-home-page/language-step/LanguageStep.spec.jsx
@@ -3,47 +3,49 @@ import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageSte
 
 const mockBtnsBox = <div>Mock Buttons Box</div>
 
-beforeEach(() => {
-  cleanup()
-  render(<LanguageStep btnsBox={mockBtnsBox} />)
-})
+describe('LanguageStep test', () => {
+  beforeEach(() => {
+    cleanup()
+    render(<LanguageStep btnsBox={mockBtnsBox} />)
+  })
 
-it('renders LanguageStep component with translated title and label', () => {
-  expect(screen.getByText('becomeTutor.languages.title')).toBeInTheDocument()
-  expect(
-    screen.getByLabelText('becomeTutor.languages.autocompleteLabel')
-  ).toBeInTheDocument()
-})
+  it('renders LanguageStep component with translated title and label', () => {
+    expect(screen.getByText('becomeTutor.languages.title')).toBeInTheDocument()
+    expect(
+      screen.getByLabelText('becomeTutor.languages.autocompleteLabel')
+    ).toBeInTheDocument()
+  })
 
-it('renders LanguageStep component without selected language', () => {
-  expect(
-    screen.queryByRole('button', { name: /clear/i })
-  ).not.toBeInTheDocument()
-})
+  it('renders LanguageStep component without selected language', () => {
+    expect(
+      screen.queryByRole('button', { name: /clear/i })
+    ).not.toBeInTheDocument()
+  })
 
-it('renders LanguageStep component with selected language', () => {
-  const selectElement = screen.getByLabelText(
-    'becomeTutor.languages.autocompleteLabel'
-  )
-  fireEvent.mouseDown(selectElement)
-  fireEvent.click(screen.getByText('English'))
-  fireEvent.mouseUp(selectElement)
+  it('renders LanguageStep component with selected language', () => {
+    const selectElement = screen.getByLabelText(
+      'becomeTutor.languages.autocompleteLabel'
+    )
+    fireEvent.mouseDown(selectElement)
+    fireEvent.click(screen.getByText('English'))
+    fireEvent.mouseUp(selectElement)
 
-  expect(screen.getByRole('button', { name: /clear/i })).toBeInTheDocument()
-})
+    expect(screen.getByRole('button', { name: /clear/i })).toBeInTheDocument()
+  })
 
-it('clears the selected language on clear button click', () => {
-  const selectElement = screen.getByLabelText(
-    'becomeTutor.languages.autocompleteLabel'
-  )
+  it('clears the selected language on clear button click', () => {
+    const selectElement = screen.getByLabelText(
+      'becomeTutor.languages.autocompleteLabel'
+    )
 
-  fireEvent.mouseDown(selectElement)
-  fireEvent.click(screen.getByText('English'))
-  fireEvent.mouseUp(selectElement)
+    fireEvent.mouseDown(selectElement)
+    fireEvent.click(screen.getByText('English'))
+    fireEvent.mouseUp(selectElement)
 
-  expect(screen.getByDisplayValue('English')).toBeInTheDocument()
+    expect(screen.getByDisplayValue('English')).toBeInTheDocument()
 
-  fireEvent.click(screen.getByRole('button', { name: /clear/i }))
+    fireEvent.click(screen.getByRole('button', { name: /clear/i }))
 
-  expect(screen.getByDisplayValue('')).toBeInTheDocument()
+    expect(screen.getByDisplayValue('')).toBeInTheDocument()
+  })
 })
