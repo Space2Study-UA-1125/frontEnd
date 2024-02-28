a4f46b7a656e3814dac43fa5346b41922b8a3a17
Vladyslava Korogodova <korogodova.vladislava@gmail.com>
2024-02-28 15:59:14 +0200

    Implemented chips for Subjects and covered it with unit tests

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -2,7 +2,8 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-
+import AppChipList from '~/components/app-chips-list/AppChipList'
+import FormHelperText from '@mui/material/FormHelperText'
 import studyCategory from '~/assets/img/tutor-home-page/become-tutor/study-category.svg'
 import AppButton from '~/components/app-button/AppButton'
 import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
@@ -16,7 +17,8 @@ const SubjectsStep = ({ btnsBox }) => {
   const { t } = useTranslation()
   const [category, setCategory] = useState(null)
   const [subject, setSubject] = useState(null)
-
+  const [selectedSubjects, setSelectedSubjects] = useState([])
+  const [subjectsError, setSubjectsError] = useState(null)
   const categoryTextFieldProps = {
     label: t('becomeTutor.categories.mainSubjectsLabel')
   }
@@ -29,9 +31,34 @@ const SubjectsStep = ({ btnsBox }) => {
     : getEmptyArrayData
 
   const handleCategoryChange = (value) => {
+    setSubjectsError(null)
     setCategory(value)
     setSubject(null)
   }
+  const handleSubjectChange = (value) => {
+    setSubjectsError(null)
+    setSubject(value)
+  }
+  const handleAddSubject = () => {
+    if (subject) {
+      const isTheSameSubjectSelected = selectedSubjects.some(
+        (selectedSubject) => subject._id === selectedSubject._id
+      )
+      if (isTheSameSubjectSelected) {
+        setSubjectsError(t('becomeTutor.categories.sameSubject'))
+        setSubject(null)
+      } else {
+        setSelectedSubjects([...selectedSubjects, subject])
+        setSubject(null)
+      }
+    }
+  }
+  const handleDeleteSubject = (chip) => {
+    setSelectedSubjects(
+      selectedSubjects.filter((subject) => subject.name !== chip)
+    )
+  }
+  const selectedSubjectsNames = selectedSubjects.map((subject) => subject.name)
 
   return (
     <Box sx={styles.container}>
@@ -56,7 +83,7 @@ const SubjectsStep = ({ btnsBox }) => {
           />
           <AsyncAutocomplete
             labelField='name'
-            onChange={(_e, newValue) => setSubject(newValue)}
+            onChange={(_e, newValue) => handleSubjectChange(newValue)}
             service={currentSubjectService}
             textFieldProps={subjectTextFieldProps}
             value={subject ? subject._id : null}
@@ -64,11 +91,21 @@ const SubjectsStep = ({ btnsBox }) => {
           />
           <AppButton
             disabled={!subject}
+            onClick={handleAddSubject}
             sx={styles.addSubjectBtn}
             variant={'tonal'}
           >
             {t('becomeTutor.categories.btnText')}
           </AppButton>
+          <FormHelperText error={subjectsError !== null}>
+            {subjectsError || null}
+          </FormHelperText>
+
+          <AppChipList
+            defaultQuantity={2}
+            handleChipDelete={handleDeleteSubject}
+            items={selectedSubjectsNames}
+          />
         </Box>
         {btnsBox}
       </Box>
