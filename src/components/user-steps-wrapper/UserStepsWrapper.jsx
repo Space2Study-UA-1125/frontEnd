ee374fb845a7711cfaea9dd79f7e75ee17c0c717
holus-bolus <ustinovoleksij@gmail.com>
2024-02-29 16:32:16 +0200

    Added the modal window opening

--- src/components/user-steps-wrapper/UserStepsWrapper.jsx
+++ src/components/user-steps-wrapper/UserStepsWrapper.jsx
@@ -1,6 +1,191 @@
-import { useEffect, useState } from 'react'
+// import { useCallback, useEffect, useState } from 'react'
+// import StepWrapper from '~/components/step-wrapper/StepWrapper'
+// import { markFirstLoginComplete } from '~/redux/reducer'
+//
+// import { StepProvider } from '~/context/step-context'
+//
+// import AddPhotoStep from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep'
+// import GeneralInfoStep from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep'
+// import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageStep'
+// import SubjectsStep from '~/containers/tutor-home-page/subjects-step/SubjectsStep'
+//
+// import { useDispatch } from 'react-redux'
+// import {
+//   initialValues,
+//   studentStepLabels,
+//   tutorStepLabels
+// } from '~/components/user-steps-wrapper/constants'
+// import { student } from '~/constants'
+// import { useModalContext } from '~/context/modal-context'
+// import PopupDialog from '~/components/popup-dialog/PopupDialog'
+// import NotificationModal from '~/containers/guest-home-page/notification-modal/NotificationModal'
+//
+// const UserStepsWrapper = ({ userRole }) => {
+//   const [isUserFetched, setIsUserFetched] = useState(false)
+//   const dispatch = useDispatch()
+//   const [modal, setModal] = useState(null)
+//   const [paperProps, setPaperProps] = useState({})
+//   const [timer, setTimer] = useState(null)
+//
+//   const { openModal } = useModalContext()
+//   const closeModal = useCallback(() => {
+//     setModal(null)
+//     setPaperProps({})
+//     setTimer(null)
+//   }, [setModal, setPaperProps, setTimer])
+//
+//   const closeModalAfterDelay = useCallback(
+//     (delay) => {
+//       const timerId = setTimeout(closeModal, delay ?? 5000)
+//       setTimer(timerId)
+//     },
+//     [closeModal]
+//   )
+//   useEffect(() => {
+//     dispatch(markFirstLoginComplete())
+//   }, [dispatch])
+//
+//   const handleCloseClick = async () => {
+//     const confirmed = await checkConfirmation({
+//       message: 'questions.unsavedChanges',
+//       title: 'titles.confirmTitle'
+//     })
+//     if (confirmed) {
+//       openModal({
+//         component: (
+//           <NotificationModal
+//             buttonTitle='Confirm'
+//             description='Are you sure you want to close?'
+//             img={info}
+//             onClose={closeModal}
+//             title='Confirmation'
+//           />
+//         )
+//       })
+//     }
+//   }
+//
+//   const childrenArr = [
+//     <GeneralInfoStep
+//       isUserFetched={isUserFetched}
+//       key='1'
+//       setIsUserFetched={setIsUserFetched}
+//     />,
+//     <SubjectsStep key='2' />,
+//     <LanguageStep key='3' />,
+//     <AddPhotoStep key='4' />
+//   ]
+//
+//   const stepLabels = userRole === student ? studentStepLabels : tutorStepLabels
+//
+//   return (
+//     <StepProvider initialValues={initialValues} stepLabels={stepLabels}>
+//       <StepWrapper onCloseClick={handleCloseClick} steps={stepLabels}>
+//         {childrenArr}
+//       </StepWrapper>
+//     </StepProvider>
+//   )
+// }
+//
+// export default UserStepsWrapper
+// import { useCallback, useEffect, useState } from 'react'
+// import StepWrapper from '~/components/step-wrapper/StepWrapper'
+// import { markFirstLoginComplete } from '~/redux/reducer'
+//
+// import { StepProvider } from '~/context/step-context'
+//
+// import AddPhotoStep from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep'
+// import GeneralInfoStep from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep'
+// import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageStep'
+// import SubjectsStep from '~/containers/tutor-home-page/subjects-step/SubjectsStep'
+//
+// import { useDispatch } from 'react-redux'
+// import {
+//   initialValues,
+//   studentStepLabels,
+//   tutorStepLabels
+// } from '~/components/user-steps-wrapper/constants'
+// import { student } from '~/constants'
+// import { useModalContext } from '~/context/modal-context'
+// import PopupDialog from '~/components/popup-dialog/PopupDialog'
+// import NotificationModal from '~/containers/guest-home-page/notification-modal/NotificationModal'
+// import useConfirm from '~/hooks/use-confirm' // Import useConfirm
+// import info from '~/assets/img/guest-home-page/info.svg' // Import info image
+//
+// const UserStepsWrapper = ({ userRole }) => {
+//   const [isUserFetched, setIsUserFetched] = useState(false)
+//   const dispatch = useDispatch()
+//   const [modal, setModal] = useState(null)
+//   const [paperProps, setPaperProps] = useState({})
+//   const [timer, setTimer] = useState(null)
+//
+//   const { openModal } = useModalContext()
+//   const { checkConfirmation } = useConfirm() // Define checkConfirmation
+//
+//   const closeModal = useCallback(() => {
+//     setModal(null)
+//     setPaperProps({})
+//     setTimer(null)
+//   }, [setModal, setPaperProps, setTimer])
+//
+//   const closeModalAfterDelay = useCallback(
+//     (delay) => {
+//       const timerId = setTimeout(closeModal, delay ?? 5000)
+//       setTimer(timerId)
+//     },
+//     [closeModal]
+//   )
+//
+//   useEffect(() => {
+//     dispatch(markFirstLoginComplete())
+//   }, [dispatch])
+//
+//   const handleCloseClick = async () => {
+//     const confirmed = await checkConfirmation({
+//       message: 'questions.unsavedChanges',
+//       title: 'titles.confirmTitle'
+//     })
+//     if (confirmed) {
+//       openModal({
+//         component: (
+//           <NotificationModal
+//             buttonTitle='Confirm'
+//             description='Are you sure you want to close?'
+//             img={info} // Use info image
+//             onClose={closeModal}
+//             title='Confirmation'
+//           />
+//         )
+//       })
+//     }
+//   }
+//
+//   const childrenArr = [
+//     <GeneralInfoStep
+//       isUserFetched={isUserFetched}
+//       key='1'
+//       onCloseClick={handleCloseClick} // Pass handleCloseClick as a prop
+//       setIsUserFetched={setIsUserFetched}
+//     />,
+//     <SubjectsStep key='2' />,
+//     <LanguageStep key='3' />,
+//     <AddPhotoStep key='4' />
+//   ]
+//
+//   const stepLabels = userRole === student ? studentStepLabels : tutorStepLabels
+//
+//   return (
+//     <StepProvider initialValues={initialValues} stepLabels={stepLabels}>
+//       <StepWrapper onCloseClick={handleCloseClick} steps={stepLabels}>
+//         {childrenArr}
+//       </StepWrapper>
+//     </StepProvider>
+//   )
+// }
+//
+// export default UserStepsWrapper
+import { useCallback, useState } from 'react'
 import StepWrapper from '~/components/step-wrapper/StepWrapper'
-import { markFirstLoginComplete } from '~/redux/reducer'
 
 import { StepProvider } from '~/context/step-context'
 
@@ -9,26 +194,52 @@ import GeneralInfoStep from '~/containers/tutor-home-page/general-info-step/Gene
 import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageStep'
 import SubjectsStep from '~/containers/tutor-home-page/subjects-step/SubjectsStep'
 
-import { useDispatch } from 'react-redux'
 import {
   initialValues,
   studentStepLabels,
   tutorStepLabels
 } from '~/components/user-steps-wrapper/constants'
 import { student } from '~/constants'
+import { useModalContext } from '~/context/modal-context'
+import ConfirmDialog from '~/components/confirm-dialog/ConfirmDialog'
+import { t } from 'i18next'
 
 const UserStepsWrapper = ({ userRole }) => {
   const [isUserFetched, setIsUserFetched] = useState(false)
-  const dispatch = useDispatch()
+  const { openModal } = useModalContext()
+  const [modal, setModal] = useState(null)
+  const [paperProps, setPaperProps] = useState({})
+  const [timer, setTimer] = useState(null)
 
-  useEffect(() => {
-    dispatch(markFirstLoginComplete())
-  }, [dispatch])
+  const closeModal = useCallback(() => {
+    setModal(null)
+    setPaperProps({})
+    setTimer(null)
+  }, [setModal, setPaperProps, setTimer])
+
+  const handleCloseClick = () => {
+    openModal({
+      component: (
+        <ConfirmDialog
+          cancelButton={t('common.no')}
+          confirmButton={t('common.yes')}
+          message={t('questions.unsavedChanges')}
+          onConfirm={() => {
+            closeModal()
+          }}
+          onDismiss={closeModal}
+          open
+          title={t('titles.confirmTitle')}
+        />
+      )
+    })
+  }
 
   const childrenArr = [
     <GeneralInfoStep
       isUserFetched={isUserFetched}
       key='1'
+      onCloseClick={handleCloseClick}
       setIsUserFetched={setIsUserFetched}
     />,
     <SubjectsStep key='2' />,
@@ -40,7 +251,9 @@ const UserStepsWrapper = ({ userRole }) => {
 
   return (
     <StepProvider initialValues={initialValues} stepLabels={stepLabels}>
-      <StepWrapper steps={stepLabels}>{childrenArr}</StepWrapper>
+      <StepWrapper onCloseClick={handleCloseClick} steps={stepLabels}>
+        {childrenArr}
+      </StepWrapper>
     </StepProvider>
   )
 }
--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
@@ -2,11 +2,30 @@ import Box from '@mui/material/Box'
 
 import { styles } from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles'
 
-const GeneralInfoStep = ({ btnsBox }) => {
+// const GeneralInfoStep = ({ btnsBox, onCloseClick }) => {
+//   return (
+//     <Box sx={styles.container}>
+//       GeneralInfo step
+//       {btnsBox}
+//       <button onClick={onCloseClick}>Close</button>
+//     </Box>
+//   )
+// }
+//
+// export default GeneralInfoStep
+
+import CloseIcon from '@mui/icons-material/Close'
+import IconButton from '@mui/material/IconButton'
+
+const GeneralInfoStep = ({ btnsBox, onCloseClick }) => {
   return (
     <Box sx={styles.container}>
       GeneralInfo step
       {btnsBox}
+      <IconButton onClick={onCloseClick}>
+        {' '}
+        <CloseIcon />
+      </IconButton>
     </Box>
   )
 }
