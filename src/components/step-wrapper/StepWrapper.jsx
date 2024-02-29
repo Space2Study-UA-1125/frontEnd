b5e64d35302bb3a3638710f60b037c53097c5164
Vladyslava Korogodova <korogodova.vladislava@gmail.com>
2024-02-29 19:42:44 +0200

    Imlemented send stepData to server

--- src/components/step-wrapper/StepWrapper.jsx
+++ src/components/step-wrapper/StepWrapper.jsx
@@ -10,13 +10,44 @@ import WestIcon from '@mui/icons-material/West'
 import AppButton from '~/components/app-button/AppButton'
 import { styles } from '~/components/step-wrapper/StepWrapper.styles'
 import useSteps from '~/hooks/use-steps'
-
+//import { useStepContext } from '~/context/step-context'
+import { studentStepLabels } from '~/components/user-steps-wrapper/constants'
 const StepWrapper = ({ children, steps }) => {
   const { activeStep, isLastStep, loading, stepOperation } = useSteps({
     steps
   })
   const { next, back, setActiveStep, handleSubmit } = stepOperation
   const { t } = useTranslation()
+  // const { stepData } = useStepContext()
+  const generalData = {
+    firstName: 'VladyslavaTest',
+    lastName: 'KorohodovaTest',
+    address: {
+      country: 'Ukraine',
+      city: 'Kyiv'
+    },
+    professionalSummary: 'SummaryTest'
+  }
+  const [generalLabel, subjectLabel, languageLabel, photoLabel] =
+    studentStepLabels
+  const stepData = {
+    [generalLabel]: generalData,
+    [subjectLabel]: ['65cb2bfc8915aeb37601c4f9', '65cb4df48915aeb37601c564'],
+    [languageLabel]: 'Ukrainian',
+    [photoLabel]: null
+  }
+  const stepDataforService = {
+    firstName: stepData[generalLabel].firstName,
+    lastName: stepData[generalLabel].lastName,
+    address: {
+      country: stepData[generalLabel].address.country,
+      city: stepData[generalLabel].address.city
+    },
+    professionalSummary: stepData[generalLabel].professionalSummary,
+    mainSubjects: stepData[subjectLabel],
+    nativeLanguage: stepData[languageLabel],
+    photo: stepData[photoLabel]
+  }
 
   const stepLabels = steps.map((step, index) => (
     <Box
@@ -32,7 +63,7 @@ const StepWrapper = ({ children, steps }) => {
   const nextButton = isLastStep ? (
     <AppButton
       loading={loading}
-      onClick={handleSubmit}
+      onClick={() => handleSubmit(stepDataforService)}
       size='small'
       sx={styles.finishBtn}
       variant='contained'
