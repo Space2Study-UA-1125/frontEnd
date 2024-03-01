059352afc8b07e5a79636e6ce140ef4fe2ad688e
Vladyslava Korogodova <korogodova.vladislava@gmail.com>
2024-03-01 08:30:04 +0200

    Fix a bug with LanguageStep component merge

--- tests/unit/containers/tutor-home-page/language-step/LanguageStep.spec.jsx
+++ tests/unit/containers/tutor-home-page/language-step/LanguageStep.spec.jsx
@@ -1,33 +1,3 @@
-import { render, screen } from '@testing-library/react'
-import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageStep'
-import { vi } from 'vitest'
-
-vi.mock('@mui/material/Box', () => ({
-  __esModule: true,
-  default: ({ children }) => <div>{children}</div>
-}))
-
-vi.mock(
-  '~/containers/tutor-home-page/language-step/LanguageStep.styles',
-  () => ({
-    styles: {
-      container: {}
-    }
-  })
-)
-
-describe('LanguageStep Component', () => {
-  it('renders the LanguageStep container', () => {
-    render(<LanguageStep />)
-    const textElement = screen.getByText(/Language step/i)
-    expect(textElement).toBeInTheDocument()
-  })
-
-  it('renders the buttons passed through the btnsBox prop', () => {
-    const btnsBoxContent = <button>Test Button</button>
-    render(<LanguageStep btnsBox={btnsBoxContent} />)
-    const btnBoxElement = screen.getByRole('button', { name: /Test Button/i })
-    expect(btnBoxElement).toBeInTheDocument()
 import { cleanup, render, screen, fireEvent } from '@testing-library/react'
 import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageStep'
 
