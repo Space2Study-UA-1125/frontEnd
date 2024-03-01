875d90591cd2be71e056d3b2fc9cb3b906fd3554
holus-bolus <ustinovoleksij@gmail.com>
2024-03-01 08:26:09 +0200

    Added the modal window

--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
@@ -1,111 +1,110 @@
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-import generalInfo from '~/assets/img/tutor-home-page/become-tutor/general-info.svg'
-import AppTextField from '~/components/app-text-field/AppTextField'
-import AppTextArea from '~/components/app-text-area/AppTextArea'
-import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
-import { styles } from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles'
-import useUserName from '~/hooks/use-user-name'
+import generalInfo from '~/assets/img/tutor-home-page/become-tutor/general-info.svg';
+import AppTextField from '~/components/app-text-field/AppTextField';
+import AppTextArea from '~/components/app-text-area/AppTextArea';
+import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete';
+import { styles } from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles';
 
-import { LocationService } from '~/services/location-service'
-import getEmptyArrayData from '~/utils/get-empty-array-data'
+import useUserName from '~/hooks/use-user-name';
 
-const GeneralInfoStep = ({ btnsBox }) => {
-  const { t } = useTranslation()
-  const { firstName, lastName, updateFirstName, updateLastName } =
-    useUserName('')
-  const [country, setCountry] = useState('')
-  const [city, setCity] = useState('')
-  const [textField, setTextField] = useState('')
+import { LocationService } from '~/services/location-service';
+import getEmptyArrayData from '~/utils/get-empty-array-data';
 
-  const countryTextFieldProps = {
-    label: t('common.labels.country')
-  }
-  const cityTextFieldProps = {
-    label: t('common.labels.city')
-  }
+const GeneralInfoStep = ({ btnsBox, onCloseClick }) => {
+    const { t } = useTranslation();
+    const { firstName, lastName, updateFirstName, updateLastName } = useUserName('');
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [textField, setTextField] = useState('');
 
-  const handleTextFieldChange = (event) => {
-    const inputValue = event.target.value
-    setTextField(inputValue)
-  }
+    const countryTextFieldProps = {
+        label: t('common.labels.country'),
+    };
+    const cityTextFieldProps = {
+        label: t('common.labels.city'),
+    };
 
-  const handleCountryChange = (value) => {
-    setCountry(value)
-    setCity(null)
-  }
+    const handleTextFieldChange = (event) => {
+        const inputValue = event.target.value;
+        setTextField(inputValue);
+    };
 
-  const handleCityChange = (value) => {
-    setCity(value)
-  }
+    const handleCountryChange = (value) => {
+        setCountry(value);
+        setCity('');
+    };
 
-  return (
-    <Box sx={styles.container}>
-      <Box sx={styles.imgContainer}>
-        <Box
-          alt='generalInfo'
-          component='img'
-          src={generalInfo}
-          sx={styles.img}
-        />
-      </Box>
+    const handleCityChange = (value) => {
+        setCity(value);
+    };
 
-      <Typography sx={styles.title} variant='subtitle1'>
-        {t('becomeTutor.generalInfo.title')}
-      </Typography>
-      <Box sx={styles.formContainer}>
-        <Box sx={styles.dataContainer}>
-          <AppTextField
-            autoFocus
-            label={t('common.labels.firstName*')}
-            name='firstName'
-            onChange={(event) => updateFirstName(event.target.value)}
-            value={firstName}
-          />
-          <AppTextField
-            autoFocus
-            label={t('common.labels.lastName*')}
-            name='lastName'
-            onChange={(event) => updateLastName(event.target.value)}
-            value={lastName}
-          />
-          <AsyncAutocomplete
-            onChange={(_e, newValue) => handleCountryChange(newValue)}
-            service={LocationService.getCountries}
-            textFieldProps={countryTextFieldProps}
-            value={country ? country : null}
-          />
-          <AsyncAutocomplete
-            onChange={(_e, newValue) => handleCityChange(newValue)}
-            service={
-              country
-                ? () => LocationService.getCities(country)
-                : getEmptyArrayData
-            }
-            textFieldProps={cityTextFieldProps}
-            value={city ? city : null}
-          />
+    return (
+        <Box sx={styles.container}>
+            <IconButton onClick={onCloseClick} sx={{ position: 'absolute', right: 8, top: 8 }}>
+                <CloseIcon />
+            </IconButton>
+            <Box sx={styles.imgContainer}>
+                <Box alt="generalInfo" component="img" src={generalInfo} sx={styles.img} />
+            </Box>
+
+            <Typography sx={styles.title} variant="subtitle1">
+                {t('becomeTutor.generalInfo.title')}
+            </Typography>
+            <Box sx={styles.formContainer}>
+                <Box sx={styles.dataContainer}>
+                    <AppTextField
+                        autoFocus
+                        label={t('common.labels.firstName*')}
+                        name='firstName'
+                        onChange={(event) => updateFirstName(event.target.value)}
+                        value={firstName}
+                    />
+                    <AppTextField
+                        autoFocus
+                        label={t('common.labels.lastName*')}
+                        name='lastName'
+                        onChange={(event) => updateLastName(event.target.value)}
+                        value={lastName}
+                    />
+                    <AsyncAutocomplete
+                        onChange={(_e, newValue) => handleCountryChange(newValue)}
+                        service={LocationService.getCountries}
+                        textFieldProps={countryTextFieldProps}
+                        value={country ? country : null}
+                    />
+                    <AsyncAutocomplete
+                        onChange={(_e, newValue) => handleCityChange(newValue)}
+                        service={
+                            country
+                                ? () => LocationService.getCities(country)
+                                : getEmptyArrayData
+                        }
+                        textFieldProps={cityTextFieldProps}
+                        value={city ? city : null}
+                    />
+                </Box>
+                <Box sx={styles.profSummaryContainer}>
+                    <AppTextArea
+                        label={t('becomeTutor.generalInfo.textFieldLabel')}
+                        maxLength={100}
+                        onChange={handleTextFieldChange}
+                        value={textField}
+                    />
+                </Box>
+                <Typography sx={styles.helperText} variant='body2'>
+                    {t('becomeTutor.generalInfo.helperText')}
+                </Typography>
+                {btnsBox}
+            </Box>
         </Box>
+    );
+};
 
-        <Box sx={styles.profSummaryContainer}>
-          <AppTextArea
-            label={t('becomeTutor.generalInfo.textFieldLabel')}
-            maxLength={100}
-            onChange={handleTextFieldChange}
-            value={textField}
-          />
-        </Box>
-        <Typography sx={styles.helperText} variant='body2'>
-          {t('becomeTutor.generalInfo.helperText')}
-        </Typography>
-        {btnsBox}
-      </Box>
-    </Box>
-  )
-}
-
-export default GeneralInfoStep
+export default GeneralInfoStep;
