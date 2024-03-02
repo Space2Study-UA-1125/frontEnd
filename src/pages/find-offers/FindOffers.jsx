bd504f33f5363534ab7a0678f8575d830c741c59
Anastasiia Ivanchenko <iva21sta@gmail.com>
2024-03-02 18:36:01 +0200

    add tutor/student offers switcher

--- src/pages/find-offers/FindOffers.jsx
+++ src/pages/find-offers/FindOffers.jsx
@@ -1,24 +1,75 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Stack from '@mui/material/Stack'
+import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+
 import AppViewSwitcher from '~/components/app-view-switcher/AppViewSwitcher'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import useUrlSearchParams from '~/hooks/use-url-search-params'
+import AppContentSwitcher from '~/components/app-content-switcher/AppContentSwitcher'
+import useAxios from '~/hooks/use-axios'
+import { offerService } from '~/services/offer-service'
+import { defaultResponses } from '~/constants'
 
 import { styles } from '~/pages/find-offers/FindOffers.styles'
 
 const FindOffers = () => {
+  const { t } = useTranslation()
   const { searchParams, setUrlSearchParams } = useUrlSearchParams()
+  const { userRole } = useSelector((state) => state.appMain)
   const [view, setView] = useState(searchParams.get('view') || 'list')
+  const [authorRole, setAuthorRole] = useState(
+    searchParams.get('authorRole') ||
+      (userRole === 'student' ? 'tutor' : 'student')
+  )
 
   useEffect(() => {
-    setUrlSearchParams({ view })
-  }, [view, setUrlSearchParams])
+    setUrlSearchParams({ view, authorRole })
+  }, [view, authorRole, setUrlSearchParams])
+
+  const serviceFunction = useCallback(
+    () => offerService.getOffers({ authorRole }),
+    [authorRole]
+  )
+
+  const { response } = useAxios({
+    service: serviceFunction,
+    defaultResponse: defaultResponses.array
+  })
+
+  const switchOptions = {
+    left: {
+      text: t('findOffers.topMenu.tutorsOffers')
+    },
+    right: {
+      text: t('findOffers.topMenu.studentsRequests')
+    }
+  }
+
+  const handleAuthorRoleChange = () => {
+    setAuthorRole(authorRole === 'student' ? 'tutor' : 'student')
+  }
 
   return (
     <PageWrapper>
       <Stack sx={styles.stack}>
+        <div />
+        <AppContentSwitcher
+          active={authorRole === 'student'}
+          onChange={handleAuthorRoleChange}
+          styles={styles.switch}
+          switchOptions={switchOptions}
+          typographyVariant={'body1'}
+        />
         <AppViewSwitcher setView={setView} view={view} />
       </Stack>
+      {response &&
+        response.items?.map(({ _id, title, authorRole }) => (
+          <div key={_id}>
+            <span>{title}</span>
+            <span>, by {authorRole}</span>
+          </div>
+        ))}
     </PageWrapper>
   )
 }
