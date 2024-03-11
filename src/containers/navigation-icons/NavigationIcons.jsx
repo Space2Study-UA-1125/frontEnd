35797e419d27217f0db2501bc5db6bd272b046cd
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-03-11 23:08:33 +0200

    Avatar

--- src/containers/navigation-icons/NavigationIcons.jsx
+++ src/containers/navigation-icons/NavigationIcons.jsx
@@ -1,10 +1,31 @@
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 
 import GuestIcons from '~/containers/navigation-icons/guest-icons/GuestIcons'
 import UserIcons from '~/containers/navigation-icons/user-icons/UserIcons'
 
+import { userService } from '~/services/user-service'
+
 const NavigationIcons = ({ setSidebarOpen }) => {
   const { userRole } = useSelector((state) => state.appMain)
+  const store = useSelector((state) => state.appMain)
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const userData = await userService.getUserById(
+        store.userId,
+        store.userRole
+      )
+      setFirstName(userData.data.firstName)
+      setLastName(userData.data.lastName)
+    }
+
+    fetchUserData()
+  }, [store.userId, store.userRole])
+  const userNameForAvatar = firstName.charAt(0) + lastName.charAt(0)
+  console.log(userNameForAvatar)
 
   if (!userRole) return <GuestIcons setSidebarOpen={setSidebarOpen} />
 
