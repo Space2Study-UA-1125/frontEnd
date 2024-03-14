e8d549f2c7d8235b230dc1b1534c7af47d8c928c
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-03-14 13:24:10 +0200

    student-home-page and tests

---
+++ src/pages/student-home/StudentHome.styles.js
@@ -0,0 +1,18 @@
+export const styles = {
+  container: {
+    marginTop: { xs: '40px', lg: '80px' },
+    display: 'flex',
+    flexDirection: 'column',
+    gap: { xs: '40px', lg: '80px' }
+  },
+  titleWithDescription: {
+    wrapper: {
+      paddingBottom: { xs: '30px', lg: '40px' },
+      textAlign: 'center'
+    },
+    title: {
+      marginBottom: '8px',
+      typography: 'h5'
+    }
+  }
+}
