7272f8f97ff7f4388b7558b9fb79fe73d34276a1
Anastasiia Ivanchenko <iva21sta@gmail.com>
2024-03-14 16:28:50 +0200

    change SearchToolbar logic to reflect URL query parameters

--- src/pages/find-offers/FindOffers.jsx
+++ src/pages/find-offers/FindOffers.jsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react'
 import Stack from '@mui/material/Stack'
 import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
+import { useLocation } from 'react-router-dom'
 
 import AppViewSwitcher from '~/components/app-view-switcher/AppViewSwitcher'
 import AppSortMenu from '~/components/app-sort-menu/AppSortMenu'
@@ -9,33 +10,43 @@ import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import useUrlSearchParams from '~/hooks/use-url-search-params'
 import AppContentSwitcher from '~/components/app-content-switcher/AppContentSwitcher'
 import useAxios from '~/hooks/use-axios'
-import { offerService } from '~/services/offer-service'
 import { defaultResponses } from '~/constants'
-
-import { styles } from '~/pages/find-offers/FindOffers.styles'
 import OfferRequestBlock from '~/components/offer-request-block/OfferRequestBlock'
 import SearchToolbar from '~/components/search-toolbar/SearchToolbar'
 import PopularCategories from '~/components/popular-categories/PopularCategories'
 import OffersContainer from '~/containers/offers-container/OffersContainer'
+import { categoryService } from '~/services/category-service'
+
+import { styles } from '~/pages/find-offers/FindOffers.styles'
 
 const FindOffers = () => {
   const { t } = useTranslation()
   const { searchParams, setUrlSearchParams } = useUrlSearchParams()
   const { userRole } = useSelector((state) => state.appMain)
 
+  const { pathname } = useLocation()
+  const codedCategoryName = pathname.split('/')[2]
+  const decodedCategoryName = decodeURIComponent(codedCategoryName)
+  const categoryName =
+    decodedCategoryName === 'offers' ? '' : decodedCategoryName
+
   const authorRole =
     searchParams.get('authorRole') ||
     (userRole === 'student' ? 'tutor' : 'student')
   const view = searchParams.get('view') || 'list'
   const sort = searchParams.get('sort') || 'createdAt'
-  const category = searchParams.get('category')
   const subject = searchParams.get('subject')
   const author = searchParams.get('author')
 
   const serviceFunction = useCallback(
     () =>
-      offerService.getOffers({ authorRole, sort, category, subject, author }),
-    [authorRole, sort, category, subject, author]
+      categoryService.getOffersByCategoryName(categoryName, {
+        authorRole,
+        sort,
+        subject,
+        author
+      }),
+    [authorRole, sort, subject, author, categoryName]
   )
 
   const { response } = useAxios({
@@ -62,7 +73,12 @@ const FindOffers = () => {
   return (
     <PageWrapper>
       <OfferRequestBlock userRole={userRole} />
-      <SearchToolbar categoryId={category} changeFilters={setUrlSearchParams} />
+      <SearchToolbar
+        author={author}
+        categoryName={categoryName}
+        changeFilters={setUrlSearchParams}
+        subjectId={subject}
+      />
       <Stack sx={styles.stack}>
         <div />
         <AppContentSwitcher
